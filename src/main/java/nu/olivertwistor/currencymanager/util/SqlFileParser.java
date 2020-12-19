ebe5b74f9c8602e215d427cd97f6ac5f4362dfea
Johan Nilsson <oliver_twistor@hotmail.com>
2020-12-19 01:43:29 +0100

    #2 Began work on a parser class for SQL files.

--- src/main/java/nu/olivertwistor/currencymanager/util/SqlFileParser.java
+++ src/main/java/nu/olivertwistor/currencymanager/util/SqlFileParser.java
@@ -1,6 +1,27 @@
 package nu.olivertwistor.currencymanager.util;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class SqlFileParser
 {
+    public SqlFileParser(final File file, final String delimiter)
+            throws FileNotFoundException, IOException
+    {
+        try (final BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(new FileInputStream(file),
+                        StandardCharsets.UTF_8)))
+        {
+            try (final Scanner scanner = new Scanner(bufferedReader))
+            {
 
+            }
+        }
+    }
 }
